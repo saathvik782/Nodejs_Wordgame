853bf10c7f3cf4ebd627c70ca46d32e8693035fe
saathvik <saathvik@samaritan.(none)>
2015-06-15 20:06:47 +0530

    made changes to socket.js and communication is working good

--- devel/app/config/socket.js
+++ devel/app/config/socket.js
@@ -13,9 +13,9 @@ module.exports = function (server) {
          * A player joins a game
          */
         socket.on('join', function (data) {
-            console.log('found join event');
-            var username = data.user;
+            var username = data.name;
             var room = data.token;
+            console.log('found join event '+username+" "+room);
 
             // If the player is the first to join, initialize the game and players array
             if (!(room in games)) {
@@ -39,13 +39,19 @@ module.exports = function (server) {
                 };
 
                 socket.join(room);
-                socket.emit('wait'); // tell the game creator to wait until a opponent joins the game
+                socket.emit('wait',{ 
+                    'display_data': 'waiting for other players..',
+                }); // tell the game creator to wait until a opponent joins the game
                 return;
             }
 
             var game = games[room];
-            if( game.status == 'ready')
-                socket.emit('missed');
+            if( game.status == 'ready'){
+                socket.emit('missed',{
+                    'display_data':'Game has already started :('
+                });
+                return;
+            }
 
             /* TODO: handle full case, a third player attempts to join the game after already 2 players has joined the game
             if (game.status === "ready") {
@@ -57,24 +63,68 @@ module.exports = function (server) {
             player.socket = socket;
             player.name = username;
             player.status = "joined";
-            var colour;
-            while(colour=randomColour() in game.colours)
-                player.colour = colour;
             player.score = 0;
+            var colour=randomColour();
+            while(colour in game.colours){
+                colour=randomColour();
+            }
+            player.colour = colour;
+            
             game.status = "waiting";
             game.players.push(player);
+            game.colours.push(colour);
              
-            io.sockets.to(room).emit('waiting-for-more-players',{ 'token':room, 'time_remaining': 0 ,'root':game.root});
+            io.sockets.to(room).emit('waiting-for-more-players',{ 
+                'token':room, 
+                'time_remaining': config.get('wordGame.gameWaitTime'),
+                'display_data': 'Game starts in '+config.get('wordGame.gameWaitTime')+' seconds',
+                'root':game.root
+            });
+            io.sockets.to(room).emit('player-joined',{
+                'display_data':username+' has joined the game',
+                'colour':colour
+            });
         });
         
         socket.on('time-remaining',function(data){
-            if(data.time_remaining == config.get('wordGame.gameWaitTime')){
-                io.sockets.to(data.token).emit('waiting-for-more-players', data);
+            if(data.time_remaining == 0){
+                //TODO: Have to implement a puzzle generator
+                
+                var game=games[data.token]
+                
+                data.player_info={};
+                for(var each in game.players){
+                    //Player scores,names and colour
+                    data.player_info[game.players[each].name]={
+                        'score':game.players[each].score,
+                        'colour':game.players[each].colour
+                    };
+                }
+                data.puzzle=[];
+                data.puzzle.push("abcdefghijklmno")
+                data.puzzle.push("abcdefghijklmno")
+                data.puzzle.push("abcdefghijklmno")
+                data.puzzle.push("abcdefghijklmno")
+                data.puzzle.push("abcdefghijklmno")
+                data.puzzle.push("abcdefghijklmno")
+                data.puzzle.push("abcdefghijklmno")
+                data.puzzle.push("abcdefghijklmno")
+                data.puzzle.push("abcdefghijklmno")
+                data.puzzle.push("abcdefghijklmno")
+                data.puzzle.push("abcdefghijklmno")
+                data.puzzle.push("abcdefghijklmno")
+                data.puzzle.push("abcdefghijklmno")
+                data.puzzle.push("abcdefghijklmno")
+                data.puzzle.push("abcdefghijklmno")
+                
+                game.status='ready';
+                io.sockets.to(data.token).emit('start', data);
             }
             else{
-                //TODO: Have to implement a puzzle generator
-                game[data.token].status='ready';
-                io.sockets.to(data.token).emit('start', data);
+                console.log("incrementing time..");
+                data.display_data='Game starts in '+data.time_remaining+' seconds';
+                io.sockets.to(data.token).emit('waiting-for-more-players', data);
+
             }
         });
         
@@ -82,6 +132,7 @@ module.exports = function (server) {
          * A player makes a new click => broadcast that move to the opponent
          */
         socket.on('new-click', function(data) {
+            var game=games[data.token];
             io.sockets.to(data.token).emit('new-click', data);
         });
 
@@ -89,7 +140,7 @@ module.exports = function (server) {
             //TODO: implement checker and send appropriate response
             io.sockets.to(data.token).emit('new-enter',{
                 'colour': data.colour,
-                'display-data': data.name+' found '+data.word
+                'display_data': data.name+' found '+data.word
             });
             var room = data.token;
             var game=games[room];
@@ -107,7 +158,7 @@ module.exports = function (server) {
             if (room in games) {
                 io.sockets.to(room).emit('player-passed', {
                     'colour': data.colour,
-                    'display-data': data.name+' passed turn'
+                    'display_data': data.name+' passed turn'
                 });
                 var game=games[room];
                 game.turn = (game.turn+1) % game.players.length;
@@ -121,17 +172,32 @@ module.exports = function (server) {
          * A player disconnects => notify opponent, leave game room and delete the game
          */
         socket.on('disconnect', function(data){
+            console.log("someone disconnected");
             for (var token in games) {
                 var game = games[token];
                 for (var i=0;i<game.players.length;i++) {
                     var player = game.players[i];
                     if (player.socket === socket) {
+                        console.log("found him "+player.colour);
                         //If there are anymore players let them know that people have disconnected
                         socket.broadcast.to(token).emit('player-disconnected', {
                             'colour': player.colour,
-                            'display-data': player.name+' disconnected'
+                            'display_data': player.name+' disconnected'
                         });
                         game.players.splice(i,1);
+                        console.log("remaining "+game.players.length); 
+                        //For handling case when all the player get disconnected 
+                        if(game.players.length <= 1){
+                            console.log('game done');
+                            game.players[0].socket.leave(token);
+                            delete game;
+                            socket.broadcast.to(token).emit('abort',{
+                                'display_data': 'Everyone got thrown off the ship'
+                            });
+                        }
+                        console.log("remaining "+game.players.length); 
+                        //For handling case when all the player get disconnected 
+                        
                         //handle if the disconnected guy has the turn
                         if(game.turn == i){
                             game.turn = (game.turn+1) % game.players.length;
@@ -139,13 +205,7 @@ module.exports = function (server) {
                                 'name' : game.players[game.turn].name
                             });
                         }
-
-                        //For handling case when all the player get disconnected 
-                        if(game.players.length <= 1){
-                            games[room].players[0].socket.leave(room);
-                            delete games[room];
-                            socket.broadcast.to(data.token).emit('abort', data);
-                        } 
+                        console.log("sent data");
                     }
                 }
             }
--- devel/app/public/js/site/game.js
+++ devel/app/public/js/site/game.js
@@ -1,3 +1,85 @@
 //This will be the utimate ai that calibrates and tinkers everything
 $(document).on('ready',function(){
+    var socket=io();
+    
+    var token,user,colour;
+    token=$('#mainScreen').attr('data-token'); 
+    user=$('#mainScreen').attr('data-user'); 
+    
+    //First event that is emitted once a game screen loads 
+    socket.emit('join',{
+        //Sending user name and token as data
+        'token':token,
+        'name':user
+    });
+
+    socket.on('wait',function(data){
+        displayOnMainScreen(data);
+    });
+    
+    socket.on('missed',function(data){
+        displayOnMainScreen(data);
+    });
+    
+    socket.on('waiting-for-more-players',function(data){
+        displayOnMainScreen(data);
+        if(data.root == user){
+            data.time_remaining--;
+            socket.emit('time-remaining',data);
+        }
+    });
+
+    socket.on('start',function(data){
+        displayOnSideScreenL(data);
+        colour=data.player_info[user].colour;
+        $('#colouredUserName').css('background-color', colour);
+        $('#mainScreen').innerHTML('');
+        //for(int i=0;i<data.puzzle.length;i++){
+        //    for(int j=0;j<data.puzzle[i].length;j++){
+        //        $('#mainScreen').append('<button class="btn btn-success disabled letter" data-letter="'+data.puzzle[i][j] +'">'+data.puzzle[i][j]+'</button>');
+        //    }
+        //    $('#mainScreen').append('<br/>')
+        //}
+    });
+
+    socket.on('new-click',function(data){
+        //TODO: highlight the correct element
+    });
+    
+    socket.on('new-turn',function(data){
+        //TODO: if not your turn make all words unclickable
+    });
+    
+    socket.on('player-passed',function(data){
+        displayOnSideScreenR(data);
+    });
+    
+    socket.on('player-joined',function(data){
+        displayOnSideScreenR(data);
+    });
+
+    
+    socket.on('player-disconnected',function(data){
+        displayOnSideScreenR(data);
+    });
+    
+    socket.on('abort',function(data){
+        displayOnMainScreen(data);
+    });
+    
+    function displayOnMainScreen(data){
+        console.log(data);
+        $('#mainScreen').text(data.display_data);
+    };
+
+    function displayOnSideScreenR(data){
+        console.log(data);
+        $('#sideScreenR').append("<font color="+data.colour+">"+data.display_data+"</font><br/>");
+    };
+    
+    function displayOnSideScreenL(data){
+        console.log(data);
+        for (var each in data.player_info)
+            $('#sideScreenL').append("<font color="+data.player_info[each].colour+">"+each+" : "+data.player_info[each].score+"</font><br/>");
+    };
 });
